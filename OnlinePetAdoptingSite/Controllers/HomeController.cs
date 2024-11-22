47504bb5224662f4d7d1a746e9d12b25e4bd5556
Furkan Bulut <furkanbtng@gmail.com>
2024-11-22 23:51:51 +0300

    contact privacy terms setted. Navbar and footer basic design

--- OnlinePetAdoptingSite/Controllers/HomeController.cs
+++ OnlinePetAdoptingSite/Controllers/HomeController.cs
@@ -23,6 +23,18 @@ public class HomeController : Controller
         return View();
     }
 
+    public IActionResult Terms()
+    {
+        return View();
+    }
+
+    public IActionResult Contact()
+    {
+        return View();
+    }
+    
+    
+
     [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
     public IActionResult Error()
     {

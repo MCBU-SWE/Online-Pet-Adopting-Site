89e5a540c470ed7d1e641612d9e7a7474586bd47
Furkan Bulut <furkanbtng@gmail.com>
2024-12-02 19:23:46 +0300

    entities added to sln

---
+++ OnlinePetAdoptingSite/entities_creation.sh
@@ -0,0 +1,6 @@
+dotnet new classlib -f net8.0 -o OnlinePetAdoptingSite/Entities
+cd OnlinePetAdoptingSite
+ dotnet sln OnlinePetAdoptingSite.sln list
+dotnet sln OnlinePetAdoptingSite.sln add Entities/
+ dotnet sln OnlinePetAdoptingSite.sln list
+

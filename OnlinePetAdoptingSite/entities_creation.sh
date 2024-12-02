176f908147fba0081f18b5f8c0ca095086810d4e
Furkan Bulut <furkanbtng@gmail.com>
2024-12-02 19:30:37 +0300

    reference added

--- OnlinePetAdoptingSite/entities_creation.sh
+++ OnlinePetAdoptingSite/entities_creation.sh
@@ -1,6 +1,7 @@
 dotnet new classlib -f net8.0 -o OnlinePetAdoptingSite/Entities
 cd OnlinePetAdoptingSite
- dotnet sln OnlinePetAdoptingSite.sln list
+dotnet sln OnlinePetAdoptingSite.sln list
 dotnet sln OnlinePetAdoptingSite.sln add Entities/
- dotnet sln OnlinePetAdoptingSite.sln list
+dotnet sln OnlinePetAdoptingSite.sln list
+dotnet add OnlinePetAdoptingSite reference Entities/
 
